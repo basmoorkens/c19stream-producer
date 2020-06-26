9534ae874e9d60c0792d111a3f5a994dd46a99fc
bas <bas.moorkens@gmail.com>
2020-06-27 00:03:16 +0200

    log to file

--- src/main/java/com/basm/c19streamer/JsonReader.java
+++ src/main/java/com/basm/c19streamer/JsonReader.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +14,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
 @Component
 public class JsonReader {
+	
+	private final static Logger log = LoggerFactory.getLogger(JsonReader.class);
 	
 	@Value("${json.file}")
 	private String fileName;
@@ -21,7 +26,7 @@ public class JsonReader {
 	public List<CovidEntry> readCovidEntries() throws JsonParseException, JsonMappingException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		List<CovidEntry> covidEntries = mapper.readValue(new File(fileName), new TypeReference<List<CovidEntry>>() {});
-		System.out.println("Found " + covidEntries.size() + " records to insert into kinesis");
+		log.info("Found " + covidEntries.size() + " records to insert into kinesis");
 		return covidEntries;
 	}
 }
--- src/main/java/com/basm/c19streamer/KinesisService.java
+++ src/main/java/com/basm/c19streamer/KinesisService.java
@@ -4,6 +4,8 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
@@ -14,17 +16,19 @@ import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
 import com.amazonaws.services.kinesis.model.PutRecordsRequest;
 import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
 import com.amazonaws.services.kinesis.model.PutRecordsResult;
-import com.amazonaws.services.kinesis.model.PutRecordsResultEntry;
 import com.amazonaws.util.StringUtils;
 
 
 @Component
 public class KinesisService {
 
+	private final static Logger log = LoggerFactory.getLogger(KinesisService.class);
+	
 	private final static String KINESIS_STREAM_NAME = "test";
 
 	public void writeCovidEntriesToKinesisStream(List<CovidEntry> covidEntries) throws InterruptedException {
 		preProcessRecords(covidEntries);
+		//fetch the aws creds from system env
 		BasicAWSCredentials awsCreds = new BasicAWSCredentials(System.getenv("aws_access_key_id"),
 				System.getenv("aws_secret_access_key"));
 		AmazonKinesis kinesisClient = AmazonKinesisClientBuilder.standard()
@@ -32,26 +36,35 @@ public class KinesisService {
 		PutRecordsRequest putRequest = new PutRecordsRequest();
 		putRequest.setStreamName(KINESIS_STREAM_NAME);
 		List<PutRecordsRequestEntry> kinesisEntries = new ArrayList<PutRecordsRequestEntry>();
-		 List<PutRecordsRequestEntry> failedRecordsList = new ArrayList<>();
+		//we will count records that fail to insert
+		int failedRecordsCounter = 0;
 		int counter = 1;
-		System.out.println("Writing " + covidEntries.size() + " covid records to stream " + KINESIS_STREAM_NAME);
+		log.info("Writing " + covidEntries.size() + " covid records to stream " + KINESIS_STREAM_NAME);
+		
+		//loop over the entries we read from json
 		for (CovidEntry covidEntry : covidEntries) {
+			//if the entry doesnt have a date just discard it
 			if(covidEntry.getDate()==null) {
 				continue;
 			}
 			PutRecordsRequestEntry kinesisEntry = new PutRecordsRequestEntry();
+			//kinesis expects its data as a byteBufferArray
 			kinesisEntry.setData(ByteBuffer.wrap((JsonUtil.convertCovidEntryToJsonBytesArray(covidEntry))));
 			kinesisEntry.setPartitionKey(covidEntry.getDate());
 			kinesisEntries.add(kinesisEntry);
+			//basic batching system, we create 500 records, push them to kinesis and then wait 1s to make sure we do not hit the rate limit
+			// the rate limit can be increased in kinesis ofc but this cost precious $$$
 			if (counter % 500 == 0 || counter == covidEntries.size()) {
 				putRequest.setRecords(kinesisEntries);
 				PutRecordsResult putRecordsResult = kinesisClient.putRecords(putRequest);
-				System.out.println("Put Result" + putRecordsResult);
+				log.info("Put Result" + putRecordsResult);
+				failedRecordsCounter += putRecordsResult.getFailedRecordCount();
 				Thread.sleep(1000);
 				kinesisEntries.clear();
 			}
 			counter++;
 		}
+		log.info("Failed to write " + failedRecordsCounter + " records to the kinesis stream");
 	}
 	
 	private void preProcessRecords(List<CovidEntry> entries) {
