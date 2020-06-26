293eb1b63231dd3d4e9e1616fbb5be824b72285b
bas <bas.moorkens@gmail.com>
2020-06-26 23:50:30 +0200

    wait for a sec after every write batch

--- src/main/java/com/basm/c19streamer/KinesisService.java
+++ src/main/java/com/basm/c19streamer/KinesisService.java
@@ -14,6 +14,7 @@ import com.amazonaws.services.kinesis.AmazonKinesisClientBuilder;
 import com.amazonaws.services.kinesis.model.PutRecordsRequest;
 import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
 import com.amazonaws.services.kinesis.model.PutRecordsResult;
+import com.amazonaws.services.kinesis.model.PutRecordsResultEntry;
 import com.amazonaws.util.StringUtils;
 
 
@@ -22,7 +23,7 @@ public class KinesisService {
 
 	private final static String KINESIS_STREAM_NAME = "test";
 
-	public void writeCovidEntriesToKinesisStream(List<CovidEntry> covidEntries) {
+	public void writeCovidEntriesToKinesisStream(List<CovidEntry> covidEntries) throws InterruptedException {
 		preProcessRecords(covidEntries);
 		BasicAWSCredentials awsCreds = new BasicAWSCredentials(System.getenv("aws_access_key_id"),
 				System.getenv("aws_secret_access_key"));
@@ -31,6 +32,7 @@ public class KinesisService {
 		PutRecordsRequest putRequest = new PutRecordsRequest();
 		putRequest.setStreamName(KINESIS_STREAM_NAME);
 		List<PutRecordsRequestEntry> kinesisEntries = new ArrayList<PutRecordsRequestEntry>();
+		 List<PutRecordsRequestEntry> failedRecordsList = new ArrayList<>();
 		int counter = 1;
 		System.out.println("Writing " + covidEntries.size() + " covid records to stream " + KINESIS_STREAM_NAME);
 		for (CovidEntry covidEntry : covidEntries) {
@@ -41,10 +43,11 @@ public class KinesisService {
 			kinesisEntry.setData(ByteBuffer.wrap((JsonUtil.convertCovidEntryToJsonBytesArray(covidEntry))));
 			kinesisEntry.setPartitionKey(covidEntry.getDate());
 			kinesisEntries.add(kinesisEntry);
-			if (counter % 100 == 0 || counter == covidEntries.size()) {
+			if (counter % 500 == 0 || counter == covidEntries.size()) {
 				putRequest.setRecords(kinesisEntries);
 				PutRecordsResult putRecordsResult = kinesisClient.putRecords(putRequest);
 				System.out.println("Put Result" + putRecordsResult);
+				Thread.sleep(1000);
 				kinesisEntries.clear();
 			}
 			counter++;
